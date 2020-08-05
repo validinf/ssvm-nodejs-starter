912b5a4062e88ca36e9da00317c1f40095dbe1f9
validinf <33304172+validinf@users.noreply.github.com>
2020-08-04 19:54:39 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn say(s: &str) -> String {
-  println!("The Rust function say() received {}", s);
-  let r = String::from("hello ");
-  return r + s;
-}
+pub fn say(s: &str) -> String { return String::from("NASA's Content of the day: ") + s; }
